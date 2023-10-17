7d228f882b6d03eedaafd4ee40b5881217776c41
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-10-17 09:30:27 +0200

    Remove Rudder and bring Ktor
    Release build is still fine.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,8 @@ android {
 }
 
 dependencies {
-  implementation("com.rudderstack.android.sdk:core:1.20.0")
+//  implementation("com.rudderstack.android.sdk:core:1.20.0")
+  implementation("io.ktor:ktor-client-okhttp:2.3.5")
 
   implementation("androidx.core:core-ktx:1.9.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
