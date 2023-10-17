ead81fa4ea44410d3c57ad33f508abb2a3d4bdfe
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-10-17 09:25:25 +0200

    Add blank new project
    (From Android Studio Giraffe 2022.3.1 Patch 2)

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+rootProject.name = "rudder-ktor-r8"
+include(":app")
+ 
